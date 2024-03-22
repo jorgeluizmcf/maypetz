b9d682d258a182a2a6d351c18fdd6fbe49b2c432
jorgeluizmcf <jorgeluizmcf@gmail.com>
2024-03-22 18:37:44 -0300

    teste de alteracao em deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Quero meus 10 mil da uniao.
         </p>
         <a
           className="App-link"
